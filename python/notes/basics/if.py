4d79dd8c3d975985b356f7e141134ee91c4c3348
Ishan <tiwathia@protonmail.com>
2022-06-25 17:19:03 +0200

    deep learning c2 , tensorflow init

--- python/notes/basics/if.py
+++
@@ -1,9 +0,0 @@
-import random
-
-a = [1,2,3,4,5]
-if   ( 1 in a ) : print("if")
-elif ( 2 in a ) : print("elif")
-else            : print("else")
-
-
-
--- python/notes/data_types/map.py
+++
@@ -1,4 +0,0 @@
-x = [1,-2,3,-4,5]
-y = map(abs,x);       # print(y) # abs each ele in list , iterable
-y = list(map(abs,x)); # print(y) # abs each ele in list , casted list 
-
